1c62ef21440b6d1b69c9618d9d3cde010c9d169f
Ricardo Atakiama <7373460-ricardo.sflabs@users.noreply.gitlab.com>
2022-01-26 12:23:06 -0300

    Uso do Hook useState para salvar o estado das tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import React, { useState } from "react";
+
 import Header from './components/Header.jsx'
 
 import './App.css';
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar React',
+      completed: false
+    },
+    {
+      id: '2',
+      title: 'Estudar para USP',
+      completed: false
+    },
+    {
+      id: '3',
+      title: 'Espairecer',
+      completed: true
+    }
+  ]);
 
   return (
     <div className='container'> 
